5428ab410a60b0f4a9cb9f89697abb41bcdcdc99
hattalo <lgd@post.cz>
2026-06-19 12:30:09 +0200

    Removed special characters from view-*.ps1 output

--- unbound/view-unbound-log.ps1
+++ unbound/view-unbound-log.ps1
@@ -1,4 +1,7 @@
-# Converts Unbound log timestamps to LOCAL time (compatible with PS 2.0+)
+﻿# PowerShell 5.x parses .ps1 files as Windows-1252 by default unless the file has a UTF-8 BOM.
+# The files needs to have the BOM encoding to be able to render dashes and emojis.
+
+# Convert Unbound log timestamps to LOCAL time (compatible with PS 2.0+)
 
 param(
     [string]$LogPath = "$PSScriptRoot\unbound.log",
@@ -19,21 +22,27 @@ Write-Host Path
 
 $outputCodePage = [Console]::OutputEncoding.CodePage
 Write-Host Console CodePage $outputCodePage
-Write-Host chcp
-chcp
 
-# Source - https://stackoverflow.com/a/57134096
-# Posted by mklement0, modified by community. See post 'Timeline' for change history
-# Retrieved 2026-06-10, License - CC BY-SA 4.0
+Write-Host UTF8.CodePage 
+Write-Host [Text.Encoding]::UTF8.CodePage
+##Write-Host chcp
+##chcp
 
-$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-Write-Host OutputEncoding $OutputEncoding
+if ($outputCodePage -ne [Text.Encoding]::UTF8.CodePage) {
+	# Source - https://stackoverflow.com/a/57134096
+	# Posted by mklement0, modified by community. See post 'Timeline' for change history
+	# Retrieved 2026-06-10, License - CC BY-SA 4.0
 
-$outputCodePage = [Console]::OutputEncoding.CodePage
-Write-Host Now Console CodePage $outputCodePage
+	$OutputEncoding = [Console]::InputEncoding = [Console]::OutputEncoding = [Text.Encoding]::UTF8
+	# $OutputEncoding = [Console]::InputEncoding = [Console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+	Write-Host OutputEncoding $OutputEncoding
 
-Write-Host "`n🪵 Unbound Log (Last $Lines lines, LOCAL time):" -ForegroundColor Cyan -BackgroundColor DarkGray
-Write-Host "────────────────────────────────────────────────────────────" -ForegroundColor Gray
+	$outputCodePage = [Console]::OutputEncoding.CodePage
+	Write-Host Now Console CodePage $outputCodePage
+}
+
+Write-Host "`n Unbound Log (Last $Lines lines, LOCAL time):" -ForegroundColor Cyan -BackgroundColor DarkGray
+Write-Host ("-" * 80) -ForegroundColor Gray
 
 Get-Content $LogPath -Tail $Lines | ForEach-Object {
     # Match [10-digit Unix timestamp] pattern
